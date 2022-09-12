d6917d6cee7d87f8ad87b1ca5d3805bb70f081da
sidmadke <siddhant.madke@gmail.com>
2022-09-12 22:38:41 +0530

    created navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,20 @@
-export default function Nav(){
-    return(
+export default function Nav() {
+    return (
         <nav>
-            <img src="" alt="LOGO"></img>
+            <img src={require("../img/thoratbags white.png")} width="180px" alt="LOGO" />
             <ul>
                 <li>Home</li>
-                <li>Categories</li>
-                <li>Blog</li>
-                <li>Account</li>
-            </ul>
-        </nav>
+                <div class="dropdown">
+                    <button class="dropbtn">Categories</button>
+                    <div class="dropdown-content">
+                        <a href=".">School Bags</a>
+                        <a href="">Gym Bags</a>
+                        <a href="">Sling Bags</a>
+                    </div>
+                </div>
+            <li>Blog</li>
+            <li>Account</li>
+        </ul>
+        </nav >
     )
 }
